f6366c85692c0130d12f459965a68de97a281fa9
saknet <andreas.andra@helsinki.fi>
2020-11-16 21:07:41 +0200

    changes displaying unitofmeasurement

--- public/load_cesium.js
+++ public/load_cesium.js
@@ -9,14 +9,14 @@ function colorByResult () {
     tileset.style = new Cesium.Cesium3DTileStyle({
         color: {
             conditions: [
-                ['${result} === null', 'color("red")'],
-                ['Number(${result}) === undefined', 'color("red")'],
-                ['Number(${result}) >= 1000', 'color("red")'],
-                ['Number(${result}) >= 500', 'color("orange")'],
-                ['Number(${result}) >= 100', 'color("yellow")'],
-                ['Number(${result}) >= 50', 'color("purple")'],
-                ['Number(${result}) >= 25', 'color("blue")'],
-                ['Number(${result}) >= 0', 'color("green")'],
+                ['${B[SPL]} === null', 'color("red")'],
+                ['Number(${B[SPL]}) === undefined', 'color("red")'],
+                ['Number(${B[SPL]}) >= 1000', 'color("red")'],
+                ['Number(${B[SPL]}) >= 500', 'color("orange")'],
+                ['Number(${B[SPL]}) >= 100', 'color("yellow")'],
+                ['Number(${B[SPL]}) >= 50', 'color("purple")'],
+                ['Number(${B[SPL]}) >= 25', 'color("blue")'],
+                ['Number(${B[SPL]}) >= 0', 'color("green")'],
                 // ['true', 'color("cyan")']
             ]
         }
